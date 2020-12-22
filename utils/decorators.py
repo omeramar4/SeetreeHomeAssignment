295283b136df2df28dd2fcdff919c653778358ca
Omer Amar <omer.amar@intel.com>
2020-12-22 19:36:58 +0200

    fix decorator

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,15 +1,16 @@
-import functools
+from functools import wraps
 import utils.cache_manager as cm
 from utils.metadata_loaders import ImagesMetadata, PolygonsLoader
 
 
 def load_decorator(images: bool = False, polygons: bool = False):
     def decorator(f):
-        @functools.wraps(f)
-        def wrapper():
+        @wraps(f)
+        def wrapper(*args, **kwargs):
             if images and cm.images_cache is None:
                 cm.images_cache = ImagesMetadata().load()
             if polygons and cm.polygons_cache is None:
                 cm.polygons_cache = PolygonsLoader().load()
+            return f(*args, **kwargs)
         return wrapper
     return decorator
